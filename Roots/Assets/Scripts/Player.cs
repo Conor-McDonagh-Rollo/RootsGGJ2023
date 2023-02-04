05002f696ab46fa000f9728b0f9d32b377a9d50e
Conor McDonagh Rollo <c00260445@itcarlow.ie>
2023-02-04 14:26:57 +0000

    init branch

--- Roots/Assets/Scripts/Player.cs
+++ Roots/Assets/Scripts/Player.cs
@@ -5,23 +5,52 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed = 10.0f;
+    public float camSpeed = 10.0f;
 
-    private Rigidbody2D rigidbody2d;
+    private Rigidbody2D rb;
+    private Transform cam;
+    private Animator anim;
+
+    private Vector3 camPos;
+    private Vector3 prevDir = Vector2.down;
 
     private void Start()
     {
-        rigidbody2d = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        cam = Camera.main.transform;
+        camPos = cam.position;
     }
 
     private void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
+        int animX = 0;
+        int animY = 0;
+        if (horizontal > 0) animX = 1;
+        else if (horizontal < 0) animX = -1;
+        if (vertical > 0) animY = 1;
+        else if (vertical < 0) animY = -1;
 
-        Vector2 position = rigidbody2d.position;
-        position.x += horizontal * speed * Time.deltaTime;
-        position.y += vertical * speed * Time.deltaTime;
+        if (horizontal != 0 || vertical != 0)
+        {
+            anim.SetBool("Walking", true);
+            anim.SetInteger("dX", animX);
+            anim.SetInteger("dY", animY);
+            prevDir = new Vector2(animX, animY);
+        }
+        else
+        {
+            anim.SetBool("Walking", false);
+            anim.SetInteger("dX", (int)prevDir.x);
+            anim.SetInteger("dY", (int)prevDir.y);
+        }
 
-        rigidbody2d.MovePosition(position);
+        rb.velocity = new Vector2(horizontal, vertical) * speed;
+        camPos = Vector3.Lerp(cam.position, rb.position, camSpeed * Time.deltaTime);
+        camPos.z = -10;
+        cam.position = camPos;
     }
+
 }
