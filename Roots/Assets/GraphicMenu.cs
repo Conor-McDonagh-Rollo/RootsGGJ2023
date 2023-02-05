9893465baf03a72bdb76ec4a4c0f6ff8995767cf
Khoa Vo <khoavo23451@gmail.com>
2023-02-05 13:10:22 +0000

    Setup Volume mixer, Adjusted resolution code

--- Roots/Assets/GraphicMenu.cs
+++ Roots/Assets/GraphicMenu.cs
@@ -16,6 +16,31 @@ public class GraphicMenu : MonoBehaviour
     void Start()
     {
         fullscreenTog.isOn = Screen.fullScreen;
+
+        bool foundRes = false;
+        for(int i = 0; i < resolutions.Count; i++) 
+        {
+            if (Screen.width == resolutions[i].horizontal && Screen.height == resolutions[i].vertical)
+            {
+                foundRes = true;
+
+                selectedResolution = i;
+
+                UpdateResLabel();
+            }
+        }
+
+        if(!foundRes)
+        {
+            ResItem newRes = new ResItem();
+            newRes.horizontal = Screen.width;
+            newRes.vertical = Screen.height;
+
+            resolutions.Add(newRes);
+            selectedResolution = resolutions.Count - 1;
+
+            UpdateResLabel();
+        }
     }
 
     // Update is called once per frame
