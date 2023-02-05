db9640a988c22c4ff8a33758b33e1ac1612babc2
Khoa Vo <khoavo23451@gmail.com>
2023-02-05 12:39:51 +0000

    Resolution & fullscreen worked

--- Roots/Assets/GraphicMenu.cs
+++ Roots/Assets/GraphicMenu.cs
@@ -2,10 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class GraphicMenu : MonoBehaviour
 {
     public Toggle fullscreenTog;
+
+    public List<ResItem> resolutions= new List<ResItem>();
+    private int selectedResolution;
+
+    public TMP_Text resolutionLabel;
 
     void Start()
     {
@@ -18,8 +24,43 @@ public class GraphicMenu : MonoBehaviour
         
     }
 
+    public void ResLeft()
+    {
+        selectedResolution--;
+        if(selectedResolution < 0)
+        {
+            selectedResolution = 0;
+        }
+
+        UpdateResLabel();
+    }
+
+    public void ResRight()
+    {
+        selectedResolution++;
+        if (selectedResolution > resolutions.Count - 1)
+        {
+            selectedResolution = resolutions.Count - 1;
+        }
+
+        UpdateResLabel();
+    }
+
+    public void UpdateResLabel()
+    {
+        resolutionLabel.text = resolutions[selectedResolution].horizontal.ToString() + " x " + resolutions[selectedResolution].vertical.ToString();
+    }
+
     public void ApplyGraphics()
     {
-        Screen.fullScreen = fullscreenTog.isOn;
+        //Screen.fullScreen = fullscreenTog.isOn;
+
+        Screen.SetResolution(resolutions[selectedResolution].horizontal, resolutions[selectedResolution].vertical, fullscreenTog.isOn);
     }
+}
+
+[System.Serializable]
+public class ResItem
+{
+    public int horizontal, vertical;
 }
